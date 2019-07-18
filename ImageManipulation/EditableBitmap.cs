0702db04225f17e7fc13041da51284133e33f0f0
Luke <oneofmanylukes@gmail.com>
2019-07-18 17:34:44 +0200

    Code cleanup, added access modifiers

--- ImageManipulation/EditableBitmap.cs
+++ ImageManipulation/EditableBitmap.cs
@@ -7,48 +7,30 @@ namespace ImageManipulation
 {
     public class EditableBitmap
     {
-        PixelFormat _format;
-        int _width;
-        int _height;
-        int _rawStride;
-        byte[] _rawImage;
-        WriteableBitmap _writableBitmap;
+        private PixelFormat _format;
+        private int _rawStride;
+        private byte[] _rawImage;
+        private WriteableBitmap _writableBitmap;
 
-        public int Width
-        {
-            get
-            {
-                return _width;
-            }
-        }
+        public int Width { get; private set; }
 
-        public int Height
-        {
-            get
-            {
-                return _height;
-            }
-        }
+        public int Height { get; private set; }
 
         public EditableBitmap(int width, int height)
         {
             _format = PixelFormats.Bgr32;
-            _width = width;
-            _height = height;
-            _rawStride = (_width * _format.BitsPerPixel + 7) / 8;
-            _rawImage = new byte[_rawStride * _height];
-            _writableBitmap = new WriteableBitmap(BitmapSource.Create(_width, _height, 96, 96, _format, null, _rawImage, _rawStride));
+            Width = width;
+            Height = height;
+            _rawStride = (Width * _format.BitsPerPixel + 7) / 8;
+            _rawImage = new byte[_rawStride * Height];
+            _writableBitmap = new WriteableBitmap(BitmapSource.Create(Width, Height, 96, 96, _format, null, _rawImage, _rawStride));
         }
 
         public void SetPixelColor(int x, int y, Color color)
         {
             VerifyCoordinates(x, y);
             Int32Rect rect = new Int32Rect(x, y, 1, 1);
-            byte[] colorData = new byte[4];
-            colorData[0] = color.B;
-            colorData[1] = color.G;
-            colorData[2] = color.R;
-            colorData[3] = color.A;
+            byte[] colorData = new byte[4] { color.B, color.G, color.R, color.A };
             _writableBitmap.WritePixels(rect, colorData, _rawStride, 0);
         }
 
@@ -59,36 +41,19 @@ namespace ImageManipulation
 
         protected bool VerifyCoordinates(int x, int y)
         {
-            bool xVerified = false;
-            bool yVerified = false;
+            bool xVerified = x >= 0 && x <= Width - 1;
+            bool yVerified = y >= 0 && y <= Height - 1;
 
-            if(x >= 0 && x <= _width - 1)
-            {
-                xVerified = true;
-            }
-            else
+            if (!xVerified)
             {
                 throw new Exception("The X coordinate passed to EditableBitmap was out of bounds");
             }
-
-            if (y >= 0 && y <= _height - 1)
-            {
-                yVerified = true;
-            }
-            else
+            else if (!yVerified)
             {
                 throw new Exception("The Y coordinate passed to EditableBitmap was out of bounds");
             }
 
-            if(xVerified && yVerified)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return xVerified && yVerified;
         }
-
     }
 }
