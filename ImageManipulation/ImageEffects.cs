8f01957118a036b84ff5fd24e10200989883565b
Andrew Yancey <andrewyanceygamedev@gmail.com>
2019-07-17 21:52:27 -0400

    Refactored image effects so it's cleaner
    refactored greyalizer, and pixelizer

--- ImageManipulation/ImageEffects.cs
+++ ImageManipulation/ImageEffects.cs
@@ -12,65 +12,14 @@ namespace ImageManipulation
     {
         public static EditableBitmap Pixelize(CopyableBitmap inputImage)
         {
-            EditableBitmap myBmp = new EditableBitmap(inputImage.Width, inputImage.Height);
-            int spacingX = 5;
-            int spacingY = 5;
-
-            for (int xa = 0; xa < inputImage.Width; xa += spacingX)
-            {
-                for (int ya = 0; ya < inputImage.Height; ya += spacingY)
-                {
-                    // decide if there is enough room for the entire spacing and 
-                    // if not then just use the remaining pixels
-                    int width = 0;
-                    int height = 0;
-                    if (ya + spacingY < inputImage.Height)
-                    {
-                        height = spacingY;
-                    }
-                    else
-                    {
-                        height = inputImage.Height - ya;
-                    }
-                    if (xa + spacingX < inputImage.Width)
-                    {
-                        width = spacingX;
-                    }
-                    else
-                    {
-                        width = inputImage.Width - xa;
-                    }
-                    Int32Rect area = new Int32Rect(xa, ya, width, height);
-                    Color averageColor = AverageColor(inputImage, area);
-                    SetColor(myBmp, area, averageColor);
-                }
-            }
-            return myBmp;
+            Pixelizer pixelizer = new Pixelizer(inputImage, 5, 5);
+            return pixelizer.Pixelize();
         }
 
         public static EditableBitmap Greyalize(CopyableBitmap inputImage)
         {
-            EditableBitmap outputImage = new EditableBitmap(inputImage.Width, inputImage.Height);
-            for (int x = 0; x < inputImage.Width; x++)
-            {
-                for (int y = 0; y < inputImage.Height; y++)
-                {
-                    Color color = inputImage.GetPixel(x, y);
-                    int newValue = 0;
-
-                    newValue += color.B;
-                    newValue += color.G;
-                    newValue += color.R;
-                    newValue += color.A;
-                    newValue = newValue / 4;
-                    color.B = (byte)newValue;
-                    color.G = (byte)newValue;
-                    color.R = (byte)newValue;
-                    color.A = (byte)newValue;
-                    outputImage.SetPixelColor(x, y, color);
-                }
-            }
-            return outputImage;
+            Greyalizer greyalizer = new Greyalizer(inputImage);
+            return greyalizer.Greyalize();
         }
 
         public static EditableBitmap Blur(CopyableBitmap inputImage)
@@ -85,39 +34,6 @@ namespace ImageManipulation
                 }
             }
             return bmp;
-        }
-
-        private static Color AverageColor(CopyableBitmap image, Int32Rect area)
-        {
-            int[] channelSums = { 0, 0, 0, 0 };
-            byte[,] pixels = (byte[,])image.GetPixelChannels(area).Clone();
-
-            for (int i = 0; i < pixels.GetLength(1); i++)
-            {
-                channelSums[0] += pixels[0, i];
-                channelSums[1] += pixels[1, i];
-                channelSums[2] += pixels[2, i];
-                channelSums[3] += pixels[3, i];
-            }
-
-            Color averageColor = new Color();
-            averageColor.B = (byte)(channelSums[0] / pixels.GetLength(1));
-            averageColor.G = (byte)(channelSums[1] / pixels.GetLength(1));
-            averageColor.R = (byte)(channelSums[2] / pixels.GetLength(1));
-            averageColor.A = (byte)(channelSums[3] / pixels.GetLength(1));
-
-            return averageColor;
-        }
-
-        private static void SetColor(EditableBitmap image, Int32Rect area, Color color)
-        {
-            for (int x = area.X; x < area.X + area.Width; x++)
-            {
-                for (int y = area.Y; y < area.Y + area.Height; y++)
-                {
-                    image.SetPixelColor(x, y, color);
-                }
-            }
         }
 
         private static List<byte[]> GetNeighbors(int x, int y, CopyableBitmap image)
